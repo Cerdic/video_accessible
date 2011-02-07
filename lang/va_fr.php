03f1d52cc6fd48e946ace8f5bca6afd0b5054557
Cerdic <cedric@yterium.com>
2011-02-07 17:24:27 +0000

    configuration du player video

--- lang/va_fr.php
+++ lang/va_fr.php
@@ -21,5 +21,46 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'label_fichier_soustitre' => 'Sous-titres',
 'label_fichier_audiodesc' => 'Audio-description',
 'label_fichier_transcript' => 'Transcription',
+
+
+'label_skin' => 'Apparence du lecteur',
+'label_skin_defaut' => 'Apparence par defaut',
+
+'label_controlbar' => 'Position de la barre de contrôle',
+'label_controlbar_bottom' => 'Barre de contrôle en bas',
+'label_controlbar_top' => 'Barre de contrôle en haut',
+'label_controlbar_over' => 'Barre de contrôle flottante',
+'label_controlbar_none' => 'Masquer la barre de contrôle',
+
+'label_autostart' => 'Autostart',
+'label_autostart_oui' => 'Démarrer automatiquement la lecture des vidéos',
+'label_autostart_non' => 'Lecture des vidéos en pause par défaut',
+
+'label_allowfullscreen' => 'Mode plein écran',
+'label_allowfullscreen_oui' => 'Autoriser le mode plein écran',
+'label_allowfullscreen_non' => 'Ne pas autoriser le mode plein écran',
+
+'label_soustitrage' => 'Sous-titres',
+'label_captions_back' => 'Fond des sous-titres',
+'label_captions_back_oui' => 'Afficher les sous-titres sur un fond sombre',
+'label_captions_back_non' => 'Incruster les sous-titres directement sur la vidéo',
+'label_captions_state' => 'Activation des sous-titres',
+'label_captions_state_oui' => 'Activer les sous-titres par défaut',
+'label_captions_state_non' => 'Desactiver les sous-titres par défaut',
+
+'label_captions_fontsize' => 'Taille de police des sous-titres',
+
+'label_audiodescription' => 'Audio-description',
+'label_audiodescription_state' => 'Activation de l\'audio-description',
+'label_audiodescription_state_oui' => 'Activer l\'audio-description par défaut',
+'label_audiodescription_state_non' => 'Desactiver l\'audio-description par défaut',
+'label_transcription' => 'Transcription',
+
+'explication_param_value' => 'Ce réglage peut être surchargé a l\'insertion du document : <tt>&lt;embXX|@param@=@val@></tt>',
+'explication_param_fichier' => 'Vous pouvez définir une url de fichier de @fonction@ lors de l\'insertion du document : <tt>&lt;embXX|@param@=@val@></tt>',
+'configurer_player_video_accessible' => 'Configurer le lecteur vidéo accessible',
+'explication_insertion_lecteur_video' => "Le lecteur vidéo est automatiquement inséré dans la page lorsque vous utilisez <tt>&lt;embXX></tt> sur un document de type vidéo, en remplaçant XX par le numéro du document.
+Le lecteur peut être personalisé avec les réglages ci-dessous."
+
 );
 ?>
