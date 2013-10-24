b6cf42d67778b49f468374301fc7d470cd1d1065
Cerdic <cedric@yterium.com>
2013-10-24 12:59:18 +0200

    bugfix : support multiversion, le champ media a ete renomme en media_defaut dans les versions recentes de SPIP (realet)

--- inc/verifier_document_mode_audiodesc.php
+++ inc/verifier_document_mode_audiodesc.php
@@ -9,7 +9,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function inc_verifier_document_mode_audiodesc_dist($infos){
 
-	$media = sql_getfetsel('media','spip_types_documents','extension='.sql_quote($infos['extension']));
+	$row = sql_fetsel('*','spip_types_documents','extension='.sql_quote($infos['extension']));
+	$media = (isset($row['media_defaut'])?$row['media_defaut']:$row['media']);
 	if ($media!=='audio')
 	  return _T('va:format_audiodesc_incorrect');
 
--- inc/verifier_document_mode_transcript.php
+++ inc/verifier_document_mode_transcript.php
@@ -9,7 +9,9 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function inc_verifier_document_mode_transcript_dist($infos){
 
-	$media = sql_getfetsel('media','spip_types_documents','extension='.sql_quote($infos['extension']));
+	$row = sql_fetsel('*','spip_types_documents','extension='.sql_quote($infos['extension']));
+	$media = (isset($row['media_defaut'])?$row['media_defaut']:$row['media']);
+
 	if (in_array($media,array('audio','video','image')))
 	  return _T('va:format_transcript_incorrect');
 
